190081ad6de75dd6f0ba7ca4f519436fbf1b6f47
lordmoocow <src@samueljwilliams.co.uk>
2024-12-04 21:47:54 +0000

    day4: resolve the little girls x-mas puzzle misunderstanding

--- internal/aoc/days/04.go
+++ internal/aoc/days/04.go
@@ -57,17 +57,33 @@ func (d *Day4) search(i, x, y, xdir, ydir int) bool {
 	if i == len(xmas) {
 		return true
 	}
-	if x < 0 || y < 0 || x >= len(d.wordSearch[0]) || y >= len(d.wordSearch) {
-		return false
-	}
-	v := d.wordSearch[y][x]
-	if rune(xmas[i]) == v {
+
+	if d.check(rune(xmas[i]), x, y) {
 		nx, ny := x+xdir, y+ydir
 		return d.search(i+1, nx, ny, xdir, ydir)
 	}
 	return false
 }
 
-func (d *Day4) PartTwo() int {
-	return 0
+func (d *Day4) check(want rune, x, y int) bool {
+	if x < 0 || y < 0 || x >= len(d.wordSearch[0]) || y >= len(d.wordSearch) {
+		return false
+	}
+	v := d.wordSearch[y][x]
+	return want == v
+}
+
+func (d *Day4) PartTwo() (result int) {
+	for y, row := range d.wordSearch {
+		for x, v := range row {
+			if v == 'A' {
+				if (d.check('M', x-1, y-1) && d.check('S', x+1, y+1)) || (d.check('S', x-1, y-1) && d.check('M', x+1, y+1)) {
+					if (d.check('M', x-1, y+1) && d.check('S', x+1, y-1)) || (d.check('S', x-1, y+1) && d.check('M', x+1, y-1)) {
+						result++
+					}
+				}
+			}
+		}
+	}
+	return result
 }
--- internal/aoc/days/04_test.go
+++ internal/aoc/days/04_test.go
@@ -17,3 +17,32 @@ func TestDayFourPartOne(t *testing.T) {
 		t.Fatalf(`Day4.PartOne got %d but expected %d`, result, want)
 	}
 }
+
+func BenchmarkDayFourPartOne(b *testing.B) {
+	d := Day4{}
+	d.Init(assets.InputData(4))
+
+	for i := 0; i < b.N; i++ {
+		d.PartOne()
+	}
+}
+
+func TestDayFourPartTwo(t *testing.T) {
+	d := Day4{}
+	d.Init(assets.TestData(4))
+
+	want := 9
+	result := d.PartTwo()
+	if result != want {
+		t.Fatalf(`Day4.PartTwo got %d but expected %d`, result, want)
+	}
+}
+
+func BenchmarkDayFourPartTwo(b *testing.B) {
+	d := Day4{}
+	d.Init(assets.InputData(4))
+
+	for i := 0; i < b.N; i++ {
+		d.PartTwo()
+	}
+}
