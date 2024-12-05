fd3a5811cd9d2adfb0443adb9bef7a8dfef3f09c
lordmoocow <src@samueljwilliams.co.uk>
2024-12-05 16:41:25 +0000

    day 5: check the print queue for valid updates

---
+++ internal/aoc/days/05_test.go
@@ -0,0 +1,29 @@
+package days
+
+import (
+	"testing"
+
+	"github.com/lordmoocow/aoc24/internal/assets"
+)
+
+func TestDayFivePartOne(t *testing.T) {
+	d := Day5{}
+	d.Init(assets.TestData(5))
+
+	want := 143
+
+	result := d.PartOne()
+	if result != want {
+		t.Fatalf(`Day5.PartOne got %d but expected %d`, result, want)
+	}
+}
+
+func BenchmarkDayFivePartOne(b *testing.B) {
+	d := Day5{}
+	d.Init(assets.InputData(5))
+
+	for i := 0; i < b.N; i++ {
+		d.PartOne()
+	}
+}
+
