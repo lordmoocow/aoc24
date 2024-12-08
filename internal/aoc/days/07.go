b514c11784db10530e466b297e2b3f974cabc523
lordmoocow <src@samueljwilliams.co.uk>
2024-12-08 11:12:53 +0000

    day7: recalibrate bridge repair

--- internal/aoc/days/07.go
+++ internal/aoc/days/07.go
@@ -1,6 +1,7 @@
 package days
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -32,7 +33,7 @@ func (d *Day7) Init(input string) {
 
 func (d *Day7) PartOne() (result int) {
 	for _, c := range d.calibrations {
-		if ok := c.solve(c.values[0], c.values[1:]); ok {
+		if ok := c.solve(c.values[0], c.values[1:], ops[:2]); ok {
 			result += c.result
 		}
 	}
@@ -41,9 +42,10 @@ func (d *Day7) PartOne() (result int) {
 
 type operator = func(a, b int) int
 
-var ops [2]operator = [2]operator{
+var ops [3]operator = [3]operator{
 	add,
 	mul,
+	cat,
 }
 
 func add(v int, n int) int {
@@ -54,18 +56,31 @@ func mul(v int, n int) int {
 	return v * n
 }
 
-func (c calibration) solve(x int, equation []int) bool {
-	if x < c.result && len(equation) > 0 {
+func (c calibration) solve(x int, equation []int, ops []operator) bool {
+	if len(equation) > 0 {
+		if x > c.result {
+			return false
+		}
 		for _, op := range ops {
 			o := op(x, equation[0])
-			if c.solve(o, equation[1:]) {
+			if c.solve(o, equation[1:], ops) {
 				return true
 			}
 		}
 	}
-	return x == c.result
+	return len(equation) == 0 && x == c.result
 }
 
 func (d *Day7) PartTwo() (result int) {
+	for _, c := range d.calibrations {
+		if ok := c.solve(c.values[0], c.values[1:], ops[:]); ok {
+			result += c.result
+		}
+	}
 	return result
+}
+
+func cat(v int, n int) int {
+	x, _ := strconv.Atoi(fmt.Sprintf("%d%d", v, n))
+	return x
 }
--- internal/aoc/days/07_test.go
+++ internal/aoc/days/07_test.go
@@ -26,3 +26,23 @@ func BenchmarkDaySevenPartOne(b *testing.B) {
 		d.PartOne()
 	}
 }
+
+func TestDaySevenPartTwo(t *testing.T) {
+	d := Day7{}
+	d.Init(assets.TestData(7))
+
+	want := 11387
+	result := d.PartTwo()
+	if result != want {
+		t.Fatalf(`Day7.PartTwo got %d but expected %d`, result, want)
+	}
+}
+
+func BenchmarkDaySevenPartTwo(b *testing.B) {
+	d := Day7{}
+	d.Init(assets.InputData(7))
+
+	for i := 0; i < b.N; i++ {
+		d.PartTwo()
+	}
+}
