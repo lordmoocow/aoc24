337ade989dbd717e863280fa889c38485f16b444
lordmoocow <src@samueljwilliams.co.uk>
2024-12-11 02:49:26 +0000

    cli: day is back to a positional arg rather than an "option"

--- cmd/aoc/cmd/run.go
+++ cmd/aoc/cmd/run.go
@@ -1,25 +1,26 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 
 	"github.com/lordmoocow/aoc24/internal/aoc"
 	"github.com/spf13/cobra"
 )
 
 var (
-	day   int
 	parts []int
 )
 
 var runCmd = &cobra.Command{
-	Use:   "run -d day [-p part]... INPUT",
+	Use:   "run <day> [--part n]... <input_file>",
 	Short: "Run an advent of code puzzle",
-	Args:  cobra.ExactArgs(1),
+	Args:  cobra.ExactArgs(2),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		input, err := parseArgs(args)
+		day, input, err := parseArgs(args)
 		if err != nil {
 			return err
 		}
@@ -37,20 +38,26 @@ var runCmd = &cobra.Command{
 	},
 }
 
-func parseArgs(args []string) (input string, err error) {
-	file, err := os.Open(args[0])
+func parseArgs(args []string) (day int, input string, err error) {
+	day, err = strconv.Atoi(args[0])
 	if err != nil {
-		return input, err
+		return day, input, err
+	}
+	if day > 25 || day < 1 {
+		return day, input, errors.New("day out of range (1-25)")
+	}
+
+	file, err := os.Open(args[1])
+	if err != nil {
+		return day, input, err
 	}
 	buf, _ := io.ReadAll(file)
 	input = string(buf)
 
-	return input, err
+	return day, input, err
 }
 
 func init() {
-	runCmd.Flags().IntVarP(&day, "day", "d", 0, "the day of the calendar to run")
-	runCmd.MarkFlagRequired("day")
 	runCmd.Flags().IntSliceVarP(&parts, "part", "p", []int{1, 2}, "the part of the challenge to run")
 	rootCmd.AddCommand(runCmd)
 }
