5146ac9c0ef1ab675233536e4c2b7418d04ceb82
lordmoocow <src@samueljwilliams.co.uk>
2024-12-12 01:02:16 +0000

    utils: generic grid implementation / parser

---
+++ internal/utils/grid.go
@@ -0,0 +1,92 @@
+package utils
+
+import "iter"
+
+type Vec struct {
+	x, y int
+}
+
+func (a *Vec) Add(b *Vec) {
+	a.x += b.x
+	a.y += b.y
+}
+
+type Point Vec
+
+func (a *Point) Add(b *Vec) Point {
+	return Point{
+		a.x + b.x,
+		a.y + b.y,
+	}
+}
+
+type GridConstraint = comparable
+
+type Grid[T GridConstraint] struct {
+	cells []T
+	width int
+}
+
+func (g *Grid[T]) Bounds() Point {
+	return Point{g.width - 1, (len(g.cells) / g.width) - 1}
+}
+
+func (g *Grid[T]) InBounds(p Point) bool {
+	b := g.Bounds()
+	return p.x >= 0 && p.x <= b.x && p.y >= 0 && p.y <= b.y
+}
+
+func (g *Grid[T]) Get(p Point) (v T, ok bool) {
+	if !g.InBounds(p) {
+		return v, false
+	}
+
+	return g.cells[g.width*p.y+p.x], true
+}
+
+func (g *Grid[T]) Set(p Point, v T) bool {
+	if !g.InBounds(p) {
+		return false
+	}
+
+	g.cells[g.width*p.y+p.x] = v
+	return true
+}
+
+func (g *Grid[T]) Find(v T) iter.Seq[Point] {
+	return func(yield func(Point) bool) {
+		for i, cell := range g.cells {
+			if cell == v {
+				p := Point{i % g.width, i / g.width}
+				if !yield(p) {
+					return
+				}
+			}
+		}
+	}
+}
+
+var (
+	up    = Vec{0, -1}
+	down  = Vec{0, 1}
+	left  = Vec{1, 0}
+	right = Vec{-1, 0}
+)
+
+func (g *Grid[T]) NeighborsStraight(p Point) iter.Seq2[Point, T] {
+	return func(yield func(Point, T) bool) {
+		neighbors := []Point{
+			p.Add(&up),
+			p.Add(&down),
+			p.Add(&left),
+			p.Add(&right),
+		}
+		for _, n := range neighbors {
+			if v, ok := g.Get(n); ok {
+				if !yield(n, v) {
+					return
+				}
+			}
+		}
+	}
+}
---
+++ internal/utils/parse_test.go
@@ -0,0 +1,50 @@
+package utils
+
+import "testing"
+
+func TestParseGrid(t *testing.T) {
+	tests := map[string]struct {
+		input      string
+		options    ParseOptions
+		cols, rows int
+	}{
+		"5x5 no space between cols, new line row": {
+			input: `12345
+12345
+12345
+12345
+12345`,
+			options: ParseOptions{col: "", row: "\n"},
+			cols:    5,
+			rows:    5,
+		},
+		"5x5 space between cols, new line row": {
+			input: `1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5`,
+			options: ParseOptions{col: " ", row: "\n"},
+			cols:    5,
+			rows:    5,
+		},
+		"1x10 space between cols, new line row": {
+			input:   `1 2 3 4 5 1 2 3 4 5`,
+			options: ParseOptions{col: " ", row: "\n"},
+			cols:    10,
+			rows:    1,
+		},
+	}
+
+	for name, test := range tests {
+		test := test
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			got := ParseGrid(test.input, test.options, func(v []rune) string { return string(v) })
+			expectedCells := test.cols * test.rows
+			if len(got.cells) != expectedCells {
+				t.Fatalf("ParseGrid(%q) returned %q; expected %q cells", test.input, got.cells, expectedCells)
+			}
+		})
+	}
+}
