56c0d6bb715885b954dace49adbaaf64522179d0
lordmoocow <src@samueljwilliams.co.uk>
2024-12-12 21:06:00 +0000

    refactor(day10): use utils Grid and Point

--- internal/aoc/days/10.go
+++ internal/aoc/days/10.go
@@ -2,30 +2,17 @@ package days
 
 import (
 	"slices"
-	"strconv"
-	"strings"
+
+	"github.com/lordmoocow/aoc24/internal/utils"
 )
 
 type Day10 struct {
-	topography    [][]int
-	width, height int
-	trails        map[vec][][]vec
+	topography utils.Grid[int]
+	trails     map[utils.Point][][]utils.Point
 }
 
 func (d *Day10) Init(input string) {
-	rows := strings.Split(strings.TrimSpace(input), "\n")
-	d.height = len(rows) - 1
-	d.topography = make([][]int, d.height+1)
-	for i, row := range rows {
-		if len(row) > 0 {
-			d.width = len(row) - 1
-			d.topography[i] = make([]int, d.width+1)
-			for j, v := range row {
-				x, _ := strconv.Atoi(string(v))
-				d.topography[i][j] = x
-			}
-		}
-	}
+	d.topography = utils.ParseIntGrid(input, utils.DefaultParseOptions)
 }
 
 func (d *Day10) PartOne() (result int) {
@@ -37,60 +24,42 @@ func (d *Day10) PartOne() (result int) {
 }
 
 func (d *Day10) getTrails(unique bool) {
-	d.trails = map[vec][][]vec{}
-	for y, row := range d.topography {
-		for x, elevation := range row {
-			if elevation == 0 {
-				xy := vec{x, y}
-				trail := make([]vec, 1, 100)
-				trail[0] = xy
-				d.followTrail(trail, unique)
-			}
-		}
+	d.trails = map[utils.Point][][]utils.Point{}
+	for xy := range d.topography.Find(0) {
+		trail := make([]utils.Point, 1, 100)
+		trail[0] = xy
+		d.followTrail(trail, unique)
 	}
 }
 
-func (d *Day10) followTrail(trail []vec, unique bool) {
+func (d *Day10) followTrail(trail []utils.Point, unique bool) {
 	current := trail[len(trail)-1]
 
-	elevation := d.topography[current.y][current.x]
+	elevation, ok := d.topography.Get(current)
+	if !ok {
+		return
+	}
 
 	if elevation == 9 {
 		d.addTrail(trail, unique)
 		return
 	}
 
-	for _, dir := range []vec{
-		{0, -1},
-		{0, 1},
-		{-1, 0},
-		{1, 0},
-	} {
-		next := vec{current.x + dir.x, current.y + dir.y}
-		if d.inbounds(next) {
-			nextElevation := d.topography[next.y][next.x]
-			if nextElevation-elevation == 1 {
-				t := make([]vec, len(trail)+1)
-				copy(t, trail)
-				t[len(t)-1] = next
-				d.followTrail(t, unique)
-			}
+	for next, nextElevation := range d.topography.NeighborsStraight(current) {
+		if nextElevation-elevation == 1 {
+			t := make([]utils.Point, len(trail)+1)
+			copy(t, trail)
+			t[len(t)-1] = next
+			d.followTrail(t, unique)
 		}
 	}
 }
 
-func (d *Day10) inbounds(point vec) bool {
-	if point.x < 0 || point.y < 0 || point.x > d.width || point.y > d.height {
-		return false
-	}
-	return true
-}
-
-func (d *Day10) addTrail(trail []vec, unique bool) {
+func (d *Day10) addTrail(trail []utils.Point, unique bool) {
 	if d.trails[trail[0]] == nil {
-		d.trails[trail[0]] = make([][]vec, 0, 100)
+		d.trails[trail[0]] = make([][]utils.Point, 0, 100)
 	}
-	if !slices.ContainsFunc(d.trails[trail[0]], func(t []vec) bool {
+	if !slices.ContainsFunc(d.trails[trail[0]], func(t []utils.Point) bool {
 		if unique {
 			return slices.Equal(t, trail)
 		}
