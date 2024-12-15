89d1e5f10700f58d36eedff032511546ec808451
lordmoocow <src@samueljwilliams.co.uk>
2024-12-15 01:50:27 +0000

    utils: queue and priority queue

---
+++ internal/utils/queue.go
@@ -0,0 +1,54 @@
+package utils
+
+import (
+	"cmp"
+	"slices"
+)
+
+type Queue[T any] struct {
+	data []T
+}
+
+func (q *Queue[T]) IsEmpty() bool {
+	return len(q.data) == 0
+}
+
+func (q *Queue[T]) Push(item T) {
+	q.data = append(q.data, item)
+}
+
+func (q *Queue[T]) Next() T {
+	item := q.data[0]
+	q.data = q.data[1:]
+	return item
+}
+
+type pqitem[T comparable] struct {
+	p    int
+	item T
+}
+
+type PriorityQueue[T comparable] struct {
+	Queue[pqitem[T]]
+}
+
+func NewPQ[T comparable](cap int) PriorityQueue[T] {
+	return PriorityQueue[T]{
+		Queue: Queue[pqitem[T]]{
+			data: make([]pqitem[T], 0, cap),
+		},
+	}
+}
+
+func (q *PriorityQueue[T]) Push(priority int, item T) {
+	i, _ := slices.BinarySearchFunc(q.data, priority, func(a pqitem[T], b int) int {
+		return cmp.Compare(a.p, b)
+	})
+	q.data = append(q.data, pqitem[T]{})
+	copy(q.data[i+1:], q.data[i:])
+	q.data[i] = pqitem[T]{priority, item}
+}
+
+func (q *PriorityQueue[T]) Next() T {
+	return q.Queue.Next().item
+}
